d562bf6f3055b571d5e87af7979537280144f67b
sergio <sergiochogamez28@hotmail.com>
2023-02-11 11:53:01 -0800

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by sergio on 11/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
