45515d641c9ee8251dba94b06eebb06709bba770
sergio2 <sergiochogamez28@hotmail.com>
2024-02-15 23:10:41 +0000

    Algunos cambios de color

--- Todo/MyColors.swift
+++ Todo/MyColors.swift
@@ -12,7 +12,7 @@ extension Color{
     // checbox color
     public static let MyBlue: Color = Color(UIColor(red:183/255, green: 208/255, blue: 225/255 , alpha: 1.0))
     // segment color
-    public static let BlueSegment: Color = Color(UIColor(red:95/255, green: 132/255, blue: 162/255 , alpha: 1.0))
+    public static let Myblack: Color = Color(UIColor(red:95/255, green: 132/255, blue: 162/255 , alpha: 1.0))
 
     
 }
